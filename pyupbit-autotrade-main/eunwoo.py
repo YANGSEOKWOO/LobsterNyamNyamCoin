a123e4091ffb5e091a5b207d9c77e6d89fd7f505
eunwoo3990 <111335314+eunwoo3990@users.noreply.github.com>
2022-10-16 14:10:07 +0900

    Update eunwoo.py

--- pyupbit-autotrade-main/eunwoo.py
+++ pyupbit-autotrade-main/eunwoo.py
@@ -40,24 +40,24 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
+        start_time = get_start_time("KRW-SOL")
         end_time = start_time + datetime.timedelta(days=1)
         wasSold = 0
 
-        target_price = get_target_price("KRW-BTC", 0.5)
-        current_price = get_current_price("KRW-BTC")
+        target_price = get_target_price("KRW-SOL", 0.5)
+        current_price = get_current_price("KRW-SOL")
         #사는 타이밍: 변동성 돌파 전력이랑 똑같음
         if target_price < current_price and wasSold == 0:
             krw = get_balance("KRW")
             if krw > 5000:
-                upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                upbit.buy_market_order("KRW-SOL", krw*0.9995)
                 wasSold = 0
         # 파는 타이밍: 5프로 손해 또는 시간 끝
         if current_price <= target_price * 0.95:
-            upbit.sell_market_order("KRW-BTC", btc * 0.9995)
+            upbit.sell_market_order("KRW-SOL", btc * 0.9995)
             wasSold = 1
         if now > end_time - datetime.timedelta(seconds=10) and wasSold != 1:
-            btc = get_balance("BTC")
+            btc = get_balance("SOL")
             if btc > 0.00008:
                 upbit.sell_market_order("KRW-BTC", btc*0.9995)
                 time.sleep(1)
