08168b711a1ab466505665fa4d9399061095035d
eunwoo3990 <111335314+eunwoo3990@users.noreply.github.com>
2022-10-16 14:28:36 +0900

    Update eunwoo.py

--- pyupbit-autotrade-main/eunwoo.py
+++ pyupbit-autotrade-main/eunwoo.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "your-access"
-secret = "your-secret"
+access = "0YjQquXVtx4VlRLLNRc1tbKR9CT6fJDgMzstwdb4"
+secret = "eN4jZxqxKBqwpkiTbxAgGFfvEtGYq0lYirZOjnxU"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
